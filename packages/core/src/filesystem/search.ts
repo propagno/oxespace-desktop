282e95579732790379b2c67fb713c92dc13efbf7
Dax Raad <d@ironbay.co>
2026-06-24 18:39:42 -0400

    fix(core): scope fff broad scanning

--- packages/core/src/filesystem/search.ts
+++ packages/core/src/filesystem/search.ts
@@ -1,5 +1,6 @@
 export * as FileSystemSearch from "./search"
 
+import os from "os"
 import path from "path"
 import { Context, Effect, Layer, Scope } from "effect"
 import { Fff } from "#fff"
@@ -127,8 +128,8 @@ export const fffLayer = Layer.effect(
         Fff.create({
           basePath: location.directory,
           aiMode: true,
-          enableFsRootScanning: true,
-          enableHomeDirScanning: true,
+          enableFsRootScanning: location.directory === path.parse(location.directory).root,
+          enableHomeDirScanning: location.directory === os.homedir(),
         }),
       catch: (cause) => cause,
     }).pipe(Effect.orDie)
